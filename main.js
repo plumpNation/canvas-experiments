94ca04ebf14abc84da75e2edcf6ebb32a2d9c52e
Gavin King <gavin.king@itslearning.com>
2016-07-03 18:38:56 +0200

    Experimenting with canvas

---
+++ main.js
@@ -0,0 +1,116 @@
+(function () {
+    let playing = false;
+
+    document.addEventListener('DOMContentLoaded', init);
+
+    function setupPlayButton() {
+        let playButton = document.getElementById('play-button');
+        let eval = () => {
+            if (playing) {
+                stop();
+
+                playButton.textContent = 'Play';
+                return;
+            }
+
+            play();
+            playButton.textContent = 'Stop';
+        };
+
+        playButton.textContent = playing ? 'Stop': 'Play';
+
+        playButton.addEventListener('click', eval);
+    }
+
+    function play() {
+        playing = true;
+
+        tick(() => {
+            let rdm1 = Math.floor(Math.random() * 500);
+            let rdm2 = Math.floor(Math.random() * 500);
+
+            outline('layer1', [
+                {x: rdm1,   y: rdm1},
+                {x: rdm2, y: rdm1},
+                {x: rdm2, y: rdm2},
+                {x: rdm1,   y: rdm2}
+            ]);
+        });
+    }
+
+    function stop() {
+        playing = false;
+    }
+
+    function init() {
+        let background = createCanvasLayer('background');
+        let layer1     = createCanvasLayer('layer1');
+
+        inheritDimensionsFromCSS('canvas');
+
+        setupPlayButton();
+    }
+
+    function tick(fn) {
+        fn();
+
+        if (playing) {
+            requestAnimationFrame(() => tick(fn));
+        }
+    }
+
+    function outline(contextId, corners, color = 'red') {
+        let context = document.getElementById(contextId).getContext('2d');
+
+        context.strokeStyle = color;
+        context.fillStyle   = 'red';
+
+        context.beginPath();
+
+        for (let j = 0; j < corners.length; ++j) {
+            let nextCorner;
+            let corner = corners[j];
+
+            context.moveTo(corner.x, corner.y);
+
+            if (j === corners.length - 1) {
+                nextCorner = corners[0];
+
+            } else {
+                nextCorner = corners[j + 1];
+            }
+
+            context.lineTo(nextCorner.x, nextCorner.y);
+        }
+
+        context.closePath();
+
+        context.stroke();
+        context.fill();
+    }
+
+    function createCanvasLayer(layerId) {
+        let context;
+        let canvas = document.createElement('canvas');
+
+        canvas.id = layerId;
+
+        context = canvas.getContext('2d');
+
+        document
+            .getElementById('layer-container')
+            .appendChild(canvas);
+
+        return context;
+    }
+
+    function inheritDimensionsFromCSS(selector) {
+        document.querySelectorAll(selector)
+            .forEach((element) => {
+                let style = window.getComputedStyle(element);
+
+                element.setAttribute('width', parseInt(style.width, 10));
+                element.setAttribute('height', parseInt(style.height, 10));
+            });
+    }
+}());
